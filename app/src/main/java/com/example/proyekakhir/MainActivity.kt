be2cb6fd95393c664b87a772b635629a68ce7331
php <panjihusnipadhila@gmail.com>
2020-12-10 22:04:41 +0700

    Nambahin fungsi2 on

--- app/src/main/java/com/example/proyekakhir/MainActivity.kt
+++ app/src/main/java/com/example/proyekakhir/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.proyekakhir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
+    lateinit var tv_hello: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MainActivity", "onStart()")
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.i("MainActivity", "onStart()")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause()")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.i("MainActivity", "onResume()")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop()")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy()")
     }
 }
