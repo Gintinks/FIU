7e89ff8ad98c86d40628dfc4652353d20e0cc7ce
php <panjihusnipadhila@gmail.com>
2020-12-13 21:43:56 +0700

    ROTATE LAYAR

--- app/src/main/java/com/example/proyekakhir/CommentActivity.kt
+++ app/src/main/java/com/example/proyekakhir/CommentActivity.kt
@@ -1,6 +1,9 @@
 package com.example.proyekakhir
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -9,12 +12,16 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.movie_comment.*
 
 
 class CommentActivity : AppCompatActivity(){
-    var comments : MutableList<MovieCommentModel> = mutableListOf()
-
     var totalRating = 0
+    private val PREF_NAME = "Shared"
+    private val KEY_ID = "Id"
+    private val KEY_NAMA = "Nama"
+    private val KEY_COMMENT = "Comment"
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.movie_comment)
@@ -23,11 +30,12 @@ class CommentActivity : AppCompatActivity(){
         var rating = bundle?.get("ratingDes").toString().toInt()
         val commentRepo = MovieCommentRepo(this, id, totalRating)
         val commentVM = MovieCommentViewModel(commentRepo)
-
         val list = findViewById<RecyclerView>(R.id.rv_main)
-            list.adapter = MovieCommentAdapter(this, commentVM.getmovieComments(), id.toString()) {}
-            list.layoutManager = LinearLayoutManager(this)
-
+        list.adapter = MovieCommentAdapter(this, commentVM.getmovieComments(), id.toString()) {}
+        list.layoutManager = LinearLayoutManager(this)
+        var nama: String = findViewById<EditText>(R.id.et_nama).text.toString()
+        var coment: String = findViewById<EditText>(R.id.et_comment_movie).text.toString()
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
         val simpleRatingBar = findViewById<View>(R.id.ratingBar) as RatingBar
 
         findViewById<Button>(R.id.btn_tambah). setOnClickListener {
@@ -35,32 +43,28 @@ class CommentActivity : AppCompatActivity(){
             totalRating +=1
             val Nrating = countRating.toInt()
             rating = countRating.toInt()
-//            tv_rating.setText(Nrating.toString())
-            var nama: String = findViewById<EditText>(R.id.et_nama).text.toString()
-            var coment: String = findViewById<EditText>(R.id.et_comment_movie).text.toString()
-            var newComment = MovieCommentModel(id, nama, coment)
+              var newComment = MovieCommentModel(id, nama, coment)
             commentRepo.tambahMovieComment(newComment)
             list.adapter = MovieCommentAdapter(this, commentVM.getmovieComments(), id.toString()) {}
             list.layoutManager = LinearLayoutManager(this)
             Toast.makeText(applicationContext, """$rating $Nrating""".trimIndent(), Toast.LENGTH_LONG).show()
         }
+        val etNamaRestore = sharedPreferences.getString(KEY_NAMA, "0")
+        val etCommentRestore = sharedPreferences.getString(KEY_COMMENT, "0")
+        et_nama.setText(etNamaRestore)
+        et_comment_movie.setText(etCommentRestore)
     }
 
-    fun initData(id:String){
-        val id_comment = resources.getStringArray(R.array.id_movie)
-        val nama = resources.getStringArray(R.array.nama)
-        val comment = resources.getStringArray(R.array.comment)
-        comments.clear()
-        for(i in id_comment.indices){
-            if(id_comment[i]==id) {
-            comments.add(MovieCommentModel(id_comment[i], nama[i], comment[i]))
-            totalRating += 1
-            } else if(totalRating==0){
-                totalRating += 1
-            }
-        }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(KEY_NAMA, et_nama.text.toString())
+        editor.putString(KEY_COMMENT, et_comment_movie.text.toString())
+        editor.apply()
     }
 
-
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+    }
 
 }
