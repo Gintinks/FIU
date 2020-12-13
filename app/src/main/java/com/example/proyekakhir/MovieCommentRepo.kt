d8476998f187dd958e808db436c977790572ce9e
Your Name <you@example.com>
2020-12-13 12:45:31 +0800

    comment

--- app/src/main/java/com/example/proyekakhir/MovieCommentRepo.kt
+++ app/src/main/java/com/example/proyekakhir/MovieCommentRepo.kt
@@ -5,7 +5,7 @@ class MovieCommentRepo {
 
 
     init {
-        movieCommentList.add(MovieCommentModel("yulianus wayan Y.R", "filmnya jelek ajg"))
+        movieCommentList.add(MovieCommentModel(1,2,"yulianus wayan Y.R", "filmnya jelek ajg"))
     }
 
     fun getmovieCommentList() = movieCommentList
